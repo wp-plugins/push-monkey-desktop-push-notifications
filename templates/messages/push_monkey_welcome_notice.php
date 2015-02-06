ef002214f0648a363396d92e18f566bb15ad412d
mowow <mowow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-06 12:25:57 +0000

    release 0.9.9.8.7; bugfixing
    git-svn-id: https://plugins.svn.wordpress.org/push-monkey-desktop-push-notifications/tags/0.9.9.8.7@1083786 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/messages/push_monkey_welcome_notice.php
@@ -0,0 +1,55 @@
+<div class="push-monkey">
+	<div class="container-fluid clearfix">
+		<div class="row">
+			<div class="col-md-10 push-monkey-welcome-notice" style="background-image:url('<?php echo $image_url; ?>');">
+				<div class="col-md-3 col-md-offset-2">
+					<h4>Welcome to Push Monkey</h4>
+					<p>
+					Thank you for using Push Monkey. Please take the time to check out the useful 
+					information under <strong>"Getting Started"</strong>. 
+					Any kind of problems you have, we'd love to help. Our monkeys are friendly
+					and have cupcakes.
+					</p>
+				</div><!-- .col- -->
+				<div class="col-md-4">
+					<h4>Getting Started</h4>
+					<ul>
+						<li>
+							<a href="http://blog.getpushmonkey.com/rolling-push-notifications-audience/?source=welcome_notice">
+								Rolling out Push Notifications to your Audience
+							</a>
+						</li>
+						<li>
+							<a href="http://blog.getpushmonkey.com/2015/01/five-ways-to-take-advantage-of-the-custom-push/?source=welcome_notice">
+								Five ways to use the custom push
+							</a>
+						</li>
+						<li>
+							<a href="http://www.getpushmonkey.com/help/?source=welcome_notice">
+								Check out our Help Center
+							</a>
+						</li>	
+					</ul>
+				</div><!-- .col- -->
+				<div class="col-md-2">
+					<h4>Did you know?</h4>
+					<ul>
+						<li>
+							<a href="http://blog.getpushmonkey.com/desktop-push-notifications-work-subscribe/?source=welcome_notice">
+								How Desktop Push Notifications Work
+							</a>
+						</li>
+						<li>
+							<a href="http://blog.getpushmonkey.com/2014/10/doctor-whats-best-way-push/?source=welcome_notice">
+								Best practices
+							</a>
+						</li>
+					</ul>
+				</div><!-- .col- -->
+				<div class="close-btn">
+					<a href="javascript:void(0);"><img src="<?php echo $close_url; ?>" /></a>
+				</div>
+			</div><!-- .col- -->
+		</div><!-- .row -->
+	</div><!-- .container-fluid -->
+</div><!-- .push-monkey -->
